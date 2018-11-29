5dd886d97c978256774b6cd45dd0e9ed2304ceba
sasjafor <john93412@gmail.com>
2018-11-29 16:05:13 +0100

    Updated and fixed NPM dependencies

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,10 @@ COPY root/ /
 # Make /config and install dependencies
 RUN echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list && \
     apt-get update && \
-    apt-get install -y --no-install-recommends  ffmpeg && \
     mkdir /config && \
     cd /usr/src/app && \
-    npm install discord.js node-opus ffmpeg-binaries simple-tts-docker --save
+    apt-get install -y --no-install-recommends libtool zlib1g-dev && \ 
+    npm install --save-prod
 
 EXPOSE 8080
 VOLUME /config
--- root/usr/src/app/bot.js
+++ root/usr/src/app/bot.js
@@ -1,28 +1,97 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+const token = process.env.AUTH_TOKEN;
+const Discord = require('discord.js');
+const speak = require('simple-tts-docker');
+const exec = require('child_process').exec;
+const fs = require('fs');
+const https = require('https');
+const client = new Discord.Client();
+var names_list = [];
+var conn = null;
+
+client.on('ready', () => {
+	console.log('I am ready!');
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: process.env.AUTH_TOKEN,
-   autorun: true
+
+client.on('voiceStateUpdate', async (oldState, newState) => {
+	let newUserChannel = newState.channel
+	let oldUserChannel = oldState.channel
+  
+	let newMember = newState.member
+	let oldMember = oldState.member
+  
+	if(newUserChannel && newUserChannel.joinable && !newMember.user.bot && !newMember.voice.selfMute && !newMember.voice.serverMute && (oldMember.voice.selfMute || oldMember.voice.serverMute)) {
+  
+		// User Joins a voice channel
+		if (!conn || conn.channel !== newUserChannel) {
+			conn = await newUserChannel.join()
+			.catch(console.error);
+			//console.log("conn="+conn);
+		}
+		announce(newMember.displayName, conn, newUserChannel);
+	} else if(oldUserChannel && (!newUserChannel || !newUserChannel.joinable)){
+		// User leaves a voice channel
+		var members = oldUserChannel.members;
+		var leave = true;
+	
+		for (let [s,m] of members) {
+			if(m !== oldMember && !m.user.bot) {
+				leave = false;
+			}
+		}
+	
+		if (leave) {
+			oldUserChannel.leave();
+			conn = null;
+		}
+	}
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+
+client.on('message', message => {
+	if (message.channel.id == '511144158975623169') {
+		if (message.content == '!newfile') {
+			var audio_file = message.attachments.first();
+			if (audio_file !== null && audio_file !== undefined) {
+				var filename = audio_file.url.split('/').pop();
+				filename = filename.replace(/_/g, ' ');
+				console.log(filename);
+				console.log(audio_file.url);
+				if (filename.includes('.wav') || filename.includes('.m4a') || filename.includes('.mp3') || filename.includes('.ogg')) {
+					var file = fs.createWriteStream("/config/audio/" + filename.slice(0,-3) + "wav");
+					var request = https.get(audio_file.url, function(response) {
+						response.pipe(file);
+					});
+				}
+			}
+		}
+	}
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Bot listens for '(cwl)'
-    var substring = "(cwl)";
-    if (message.indexOf(substring) !== -1) {
-        bot.sendMessage({
-            to: channelID,
-            message: ':joy:'
-        });
-    }
-});
+
+client.login(token);
+
+function announce(name, connection, channel) {
+	path = "/config/audio/" + name + ".wav";
+	if (names_list.indexOf(name) <= -1) {
+		fs.stat(path, function(err, stats) {
+			if(err) {
+				console.log('didn\'t find file:' + path);
+				speak(name, {format:'wav', filename:'/config/audio/'+name});
+				setTimeout(function() {names_list.push(name); }, 2000);
+			} else {
+				names_list.push(name);
+				console.log("path="+path);
+			}
+		});
+	}
+	const intent = connection.play(path);
+	intent.on('start', () => {
+		console.log("playing " + name);
+		console.log("list="+names_list);
+    });
+	intent.once('end', () => {
+		intent.destroy();
+	});
+    intent.once('error', errWithFile => {
+        console.log("file missing: " + errWithFile);
+		intent.destroy();
+    });
+}
