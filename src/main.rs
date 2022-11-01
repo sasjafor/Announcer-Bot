2690d83fe3380ee80b133c1c6d5c6a3e24beefbb
Sascha Forster <sascha.forster@hotmail.ch>
2022-11-01 13:24:34 +0100

    update dependencies

--- src/main.rs
+++ src/main.rs
@@ -206,7 +206,7 @@ async fn on_error(error: poise::FrameworkError<'_, Data, PError>) {
     // They are many errors that can occur, so we only handle the ones we want to customize
     // and forward the rest to the default handler
     match error {
-        poise::FrameworkError::Setup { error } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
         poise::FrameworkError::Command { error, ctx } => {
             println!("Error in command `{}`: {:?}", ctx.command().name, error,);
         }
